e61669ca037a1a791ac728612b3ab355de51e12a
Medina3005 <114082560+Medina3005@users.noreply.github.com>
2025-02-12 16:19:19 -0600

    Update branching.py
    Determines if a time traveler is from the past, present, or future.

--- branching.py
+++ branching.py
@@ -1,13 +1,12 @@
-# A time traveler has suddenly appeared in your classroom!
+# branching.py
+# by Medina Kubanychbekova
+# Date: 02/12/2025
+#Description: A time traveler program that categorizes the user into past, present, or future based on their year of origin.
 
-# Create a variable representing the traveler's
-# year of origin (e.g., year = 2000)
-# and greet our strange visitor with a different message
-# if he is from the distant past (before 1900),
-# the present era (1900-2020) or from the far future (beyond 2020).
-
+# Ask user for their year of origin
 year = int(input("Greetings! What is your year of origin? "))
 
+# Determine the time period based on the input year
 if year < 1900:
     print("Woah, that's the past!")
 elif 1900 <= year < 2020:
@@ -15,12 +14,8 @@ elif 1900 <= year < 2020:
 else:
     print("Far out, that's the future!!")
 
-
-
-# Errors:
 # Incorrect syntax in year assignment (== → =).
 # Incorrect syntax in input() function call (int.input() → int(input())).
 # Missing colon (:) in if and elif.
 # Incorrect && operator in Python (&& → and).
-# Fixed final elif condition.
-
+# Fixed final elif condition
