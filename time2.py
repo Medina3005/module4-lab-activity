e83660e1e90315319e1d477eb5db8778c954b10f
Medina3005 <114082560+Medina3005@users.noreply.github.com>
2025-02-12 16:31:17 -0600

    Update time2.py
    Similar to `time.py` but ensures 24-hour format

--- time2.py
+++ time2.py
@@ -1,10 +1,20 @@
+# time2.py
+# by Medina Kubanychbekova
+# Date: 02/12/2025
+# Description: This program asks the user for the current time and a wait time, then calculates the time the alarm will go off.
+
+# Get user input for current time and wait time
 str_time = input("What time is it now? ")
 str_wait_time = input("What is the number of hours to wait? ")
 
+# Convert input to integers
 time = int(str_time)
 wait_time = int(str_wait_time)
 
+# Calculate time when the alarm will go off
 time_when_alarm_go_off = (time + wait_time) % 24
+
+# Print result
 print(f"The alarm will go off at {time_when_alarm_go_off}:00")
 
 
