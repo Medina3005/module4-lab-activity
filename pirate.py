c5ca3b04ba3677385a74991080460b4d63a1b684
Medina3005 <114082560+Medina3005@users.noreply.github.com>
2025-02-12 16:27:56 -0600

    pirate.py
    A simple program that responds differently based on user input

--- pirate.py
+++ pirate.py
@@ -1,3 +1,17 @@
+ pirate.py
+# by Medina Kubanychbekova
+# Date: 02/12/2025
+# Description: A simple pirate-themed password checker.
+
+# Ask user for a greeting (password)
+greeting = input("Hello, possible pirate! What's the password? ")
+
+# Check if the user is a pirate
+if greeting in ["Arrr!"]:
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
+
 greeting = input("Hello, possible pirate! What's the password? ")
 
 if greeting in ["Arrr!"]:
